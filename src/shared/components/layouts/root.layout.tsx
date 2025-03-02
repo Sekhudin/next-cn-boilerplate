85cbe0c3a9f4c3e0dc2d81ddfc0cb719e88f0817
Sekhudin <sekhudinuap@gmail.com>
2025-03-03 01:07:40 +0700

    remove main layout and create root layout as top level of layout

---
+++ src/shared/components/layouts/root.layout.tsx
@@ -0,0 +1,25 @@
+import ThemeProvider from "../core/theme.provider";
+import { geist, geistMono } from "src/shared/fonts/local.font";
+import CommonToaster from "../core/common.toaster";
+import type { LayoutProps } from "src/types/global.type";
+import "src/shared/styles/globals.css";
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geist.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <CommonToaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;
