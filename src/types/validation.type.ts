ae85a5dd0644298f551024d513ffdb17c7c65991
Sekhudin <sekhudinuap@gmail.com>
2025-03-03 22:38:25 +0700

    chore: just update type validation

--- src/types/validation.type.ts
+++ src/types/validation.type.ts
@@ -1,5 +1,5 @@
-import type { StandardSchemaV1 } from "@standard-schema/spec";
 import type { ZodIssue } from "zod";
+import type { StandardSchemaV1 } from "@standard-schema/spec";
 
 export type Dto<Schema extends StandardSchemaV1> = StandardSchemaV1.InferInput<Schema>;
 export type ErrorMessage = {
