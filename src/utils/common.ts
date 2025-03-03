b8f8ec843e4a57d09115a8e1476be31aea896bc2
Sekhudin <sekhudinuap@gmail.com>
2025-03-03 15:16:11 +0700

    uti: add base util

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export * from "./base.util";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
