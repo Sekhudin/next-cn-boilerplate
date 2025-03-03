af2f859afd2043610c3b33692be8851d97798c71
Sekhudin <sekhudinuap@gmail.com>
2025-03-03 14:19:46 +0700

    solve type error

--- src/types/global.type.ts
+++ src/types/global.type.ts
@@ -2,7 +2,7 @@ import type React from "react";
 import type { NextFont, NextFontWithVariable } from "next/dist/compiled/@next/font";
 
 export type Font = NextFont | NextFontWithVariable;
-export type Props<T extends Record<string, unknown> = {}> = {
+export type Props<T extends Record<string, unknown> = Record<string, unknown>> = {
   className?: string;
 } & T;
 export type LayoutProps = Readonly<{ children: React.ReactNode }>;
