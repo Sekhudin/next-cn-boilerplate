557138e6b28b980c8d92caf96b6d6f32e4cb9926
Sekhudin <sekhudinuap@gmail.com>
2025-03-03 22:38:45 +0700

    chore: just update type validation

--- src/types/global.type.ts
+++ src/types/global.type.ts
@@ -1,4 +1,5 @@
 import type React from "react";
+import type { ExternalToast } from "sonner";
 import type { NextFont, NextFontWithVariable } from "next/dist/compiled/@next/font";
 
 export type Font = NextFont | NextFontWithVariable;
@@ -6,3 +7,11 @@ export type Props<T extends Record<string, unknown> = Record<string, unknown>> =
   className?: string;
 } & T;
 export type LayoutProps = Readonly<{ children: React.ReactNode }>;
+
+export type ToastLevel = "success" | "info" | "warning" | "error";
+export type ToastTitle = (() => React.ReactNode) | React.ReactNode;
+export type ToastNotification = {
+  level: ToastLevel;
+  title: ToastTitle;
+  data?: ExternalToast;
+};
