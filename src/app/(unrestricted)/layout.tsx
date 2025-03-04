d238f1d133d266e533f966b7a0ebcb1690fb4c8c
Sekhudin <sekhudinuap@gmail.com>
2025-03-04 14:24:18 +0700

    route: change route and source import

--- src/app/(unrestricted)/layout.tsx
+++ src/app/(unrestricted)/layout.tsx
@@ -1,3 +1,4 @@
 import Layout from "src/pages/main/main.layout";
 
+export * from "src/metadata/main.meta";
 export default Layout;
--- src/app/signin/layout.tsx
+++ src/app/signin/layout.tsx
@@ -1,3 +1,4 @@
 import Layout from "src/pages/signin/signin.layout";
 
+export * from "src/metadata/signin.meta";
 export default Layout;
