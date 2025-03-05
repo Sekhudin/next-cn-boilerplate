74e2d907d16539b387a841ce213c95021d97a5ab
Sekhudin <sekhudinuap@gmail.com>
2025-03-05 23:44:55 +0700

    minor change

--- src/pages/dashboard/dashboard.page.tsx
+++ src/pages/dashboard/dashboard.page.tsx
@@ -1,5 +1,15 @@
+"use client";
+import { useAuthStore } from "src/stores/auth.store";
+
 const Page = () => {
-  return <div className="">Dashboard page</div>;
+  const authStore = useAuthStore();
+  return (
+    <div className="flex flex-col gap-y-4 px-4">
+      <p>Token: {authStore.token}</p>
+      <button onClick={() => authStore.setToken("dlflInmdimk")}>Set Token</button>
+      <button onClick={() => authStore.clearToken()}>Clear Token</button>
+    </div>
+  );
 };
 
 export default Page;
