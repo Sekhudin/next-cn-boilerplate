6b8ab298f38f1fa4735963169e8fba1281142fcb
Sekhudin <sekhudinuap@gmail.com>
2025-03-05 15:27:54 +0700

    chore: update app config, has no effect with existing code

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -1,3 +1,5 @@
+import app from "../../package.json";
 export * as server from "./env/server.env";
 export * as client from "./env/client.env";
 export { cwd, isMatch, isProduction, pathDir, pathJoin, split, NODE_ENV } from "./env/util";
+export { app };
--- src/configs/env/client.env.ts
+++ src/configs/env/client.env.ts
@@ -1,3 +1,11 @@
 import { env } from "./util";
 
-export const APP_NAME = env(process.env.NEXT_PUBLIC_APP_NAME, "app-name");
+export const BACKEND_URL = env(process.env.NEXT_PUBLIC_BACKEND_URL, "");
+export const BACKEND_AUTH_ROUTE = env(process.env.NEXT_PUBLIC_BACKEND_AUTH_ROUTE, "/signin");
+export const BACKEND_AUTH_FALLBACK_ROUTE = env(
+  process.env.NEXT_PUBLIC_BACKEND_AUTH_FALLBACK_ROUTE,
+  "/token",
+);
+
+export const TERMS_ROUTE = env(process.env.NEXT_PUBLIC_TERMS_ROUTE, "/terms");
+export const POLICY_ROUTE = env(process.env.NEXT_PUBLIC_POLICY_ROUTE, "/policy");
