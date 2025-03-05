c124af9f64d1d8ff4fa7f01ec447ccee16144339
Sekhudin <sekhudinuap@gmail.com>
2025-03-05 15:30:29 +0700

    update layout and main page components

--- src/pages/main/main.layout.tsx
+++ src/pages/main/main.layout.tsx
@@ -1,8 +1,45 @@
+import { Github } from "lucide-react";
+import { AnimatedGridBackground } from "src/shared/components/backgrounds/animated-grid.background";
 import CommonLayout from "src/shared/components/layouts/common.layout";
+import ThemeToggle from "src/shared/components/toggles/theme.toggle";
+import { AnchorButton } from "src/shared/ui/anchor-button";
+import { cn } from "src/utils/common";
 import type { LayoutProps } from "src/types/global.type";
+import * as env from "src/configs/env.config";
 
 export const Layout = ({ children }: LayoutProps) => (
-  <CommonLayout className="">{children}</CommonLayout>
+  <CommonLayout className="relative w-full h-dvh overflow-y-hidden">
+    <div className="absolute top-4 right-6 flex gap-x-2">
+      <AnchorButton
+        className="rounded-full"
+        href={env.app.repository.url}
+        target="_blank"
+        variant="ghost"
+        size="icon"
+      >
+        <Github />
+      </AnchorButton>
+      <ThemeToggle />
+    </div>
+
+    <div className="container mx-auto">
+      <div className="flex gap-8 py-20 lg:py-40 items-center justify-center flex-col">
+        {children}
+      </div>
+
+      <AnimatedGridBackground
+        width={100}
+        height={80}
+        numSquares={30}
+        maxOpacity={0.1}
+        duration={5}
+        className={cn(
+          "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+        )}
+      />
+    </div>
+  </CommonLayout>
 );
 
 export default Layout;
--- src/pages/main/main.page.tsx
+++ src/pages/main/main.page.tsx
@@ -1,13 +1,71 @@
-import ThemeToggle from "src/shared/components/toggles/theme.toggle";
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { MoveRight, BookOpen } from "lucide-react";
+import { Button } from "src/shared/ui/button";
+import { AnchorButton } from "src/shared/ui/anchor-button";
+import * as Paragraph from "src/shared/components/typography/paragraph.typo";
 
 const Page = () => {
+  const [titleNumber, setTitleNumber] = React.useState(0);
+  const titles = React.useMemo(() => ["amazing", "new", "wonderful", "beautiful", "smart"], []);
+
+  React.useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (titleNumber === titles.length - 1) {
+        setTitleNumber(0);
+      } else {
+        setTitleNumber(titleNumber + 1);
+      }
+    }, 2000);
+    return () => clearTimeout(timeoutId);
+  }, [titleNumber, titles]);
+
   return (
-    <div className="">
-      Main Page
+    <React.Fragment>
       <div>
-        <ThemeToggle />
+        <Button variant="secondary" size="sm" className="gap-4">
+          Read our launch article <MoveRight className="w-4 h-4" />
+        </Button>
       </div>
-    </div>
+      <div className="flex gap-4 flex-col">
+        <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
+          <span className="text-spektr-cyan-50">This is something</span>
+          <span className="relative flex w-full justify-center overflow-hidden text-center md:pb-4 md:pt-1">
+            &nbsp;
+            {titles.map((title, index) => (
+              <motion.span
+                className="absolute font-semibold text-primary"
+                key={index}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ type: "spring", stiffness: 50 }}
+                animate={
+                  titleNumber === index
+                    ? { y: 0, opacity: 1 }
+                    : { y: titleNumber > index ? -150 : 150, opacity: 0 }
+                }
+              >
+                {title}
+              </motion.span>
+            ))}
+          </span>
+        </h1>
+
+        <Paragraph.Muted className="text-lg md:text-xl max-w-2xl text-center">
+          Managing a small business today is already tough. Avoid further complications by ditching
+          outdated, tedious trade methods. Our goal is to streamline SMB trade, making it easier and
+          faster than ever.
+        </Paragraph.Muted>
+      </div>
+      <div className="flex flex-row gap-3">
+        <AnchorButton className="gap-4" size="lg" variant="outline" href="/docs">
+          Read Docs <BookOpen className="size-4" />
+        </AnchorButton>
+        <AnchorButton className="gap-4" size="lg" href="/signin">
+          Signin Here <MoveRight className="size-4" />
+        </AnchorButton>
+      </div>
+    </React.Fragment>
   );
 };
 
