23ec28a7144bbf3510e904742a741f9a74f58c19
Sekhudin <sekhudinuap@gmail.com>
2025-03-05 16:56:45 +0700

    minor change

--- src/configs/env/client.env.ts
+++ src/configs/env/client.env.ts
@@ -1,6 +1,6 @@
 import { env } from "./util";
 
-export const BACKEND_URL = env(process.env.NEXT_PUBLIC_BACKEND_URL, "");
+export const BACKEND_URL = env(process.env.NEXT_PUBLIC_BACKEND_URL, "https://backend.com");
 export const BACKEND_AUTH_ROUTE = env(process.env.NEXT_PUBLIC_BACKEND_AUTH_ROUTE, "/signin");
 export const BACKEND_AUTH_FALLBACK_ROUTE = env(
   process.env.NEXT_PUBLIC_BACKEND_AUTH_FALLBACK_ROUTE,
