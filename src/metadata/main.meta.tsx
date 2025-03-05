41e69d4917cee20797b7ab625ef04cfa72410098
Sekhudin <sekhudinuap@gmail.com>
2025-03-05 15:28:29 +0700

    metadata: update metadata

--- src/metadata/main.meta.tsx
+++ src/metadata/main.meta.tsx
@@ -2,6 +2,6 @@ import { initMetadata } from "src/utils/metadata.util";
 import * as env from "src/configs/env.config";
 
 export const metadata = initMetadata({
-  title: `${env.client.APP_NAME} | Rising star`,
-  description: `${env.client.APP_NAME} is new application that provide good development standard.`,
+  title: `${env.app.name} | Rising star`,
+  description: `${env.app.name} is new application that provide good development standard.`,
 });
--- src/metadata/signin.meta.tsx
+++ src/metadata/signin.meta.tsx
@@ -2,6 +2,6 @@ import { initMetadata } from "src/utils/metadata.util";
 import * as env from "src/configs/env.config";
 
 export const metadata = initMetadata({
-  title: `${env.client.APP_NAME} | Signin`,
-  description: `${env.client.APP_NAME} is new application that provide good development standard.`,
+  title: `${env.app.name} | Signin`,
+  description: `${env.app.name} is new application that provide good development standard.`,
 });
