dec49d384830b0a0be4bca13a199f98a6b3f5b6f
Sekhudin <sekhudinuap@gmail.com>
2025-03-05 15:30:06 +0700

    update layout and signup page components

--- src/pages/signup/signup.layout.tsx
+++ src/pages/signup/signup.layout.tsx
@@ -1,8 +1,23 @@
 import CommonLayout from "src/shared/components/layouts/common.layout";
+import { Anchor } from "src/shared/ui/anchor";
 import type { LayoutProps } from "src/types/global.type";
+import * as env from "src/configs/env.config";
 
 const Layout = ({ children }: LayoutProps) => {
-  return <CommonLayout className="">{children}</CommonLayout>;
+  return (
+    <CommonLayout className="flex min-h-dvh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <div className={"flex flex-col gap-6"}>
+          {children}
+          <div className="text-balance text-center text-xs text-muted-foreground">
+            By clicking continue, you agree to our{" "}
+            <Anchor href={env.client.TERMS_ROUTE}>Terms of Service</Anchor> and{" "}
+            <Anchor href={env.client.POLICY_ROUTE}>Privacy Policy</Anchor>.
+          </div>
+        </div>
+      </div>
+    </CommonLayout>
+  );
 };
 
 export default Layout;
--- src/pages/signup/signup.page.tsx
+++ src/pages/signup/signup.page.tsx
@@ -1,5 +1,42 @@
+import React from "react";
+import NextImage from "next/image";
+import { Card, CardContent } from "src/shared/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "src/shared/ui/avatar";
+import SignUpForm from "src/modules/auth/forms/signup.form";
+import { SigInBg } from "src/shared/public/images";
+import { Favicon } from "src/shared/public/brand";
+import * as env from "src/configs/env.config";
+
 const Page = () => {
-  return <div>Signup Page</div>;
+  return (
+    <React.Fragment>
+      <div className="flex justify-center gap-2 md:justify-start">
+        <a href="/" className="flex items-center gap-2 font-medium">
+          <div className="flex h-6 w-6 items-center justify-center text-primary-foreground">
+            <Avatar className="size-8">
+              <AvatarImage src={Favicon.src} />
+              <AvatarFallback>KB</AvatarFallback>
+            </Avatar>
+          </div>
+          <span className="first-letter:uppercase font-medium">{env.app.name}</span>
+        </a>
+      </div>
+
+      <Card className="overflow-hidden">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <SignUpForm />
+          <div className="relative hidden bg-muted md:block">
+            <NextImage
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              src={SigInBg}
+              alt="Image"
+              priority={true}
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </React.Fragment>
+  );
 };
 
 export default Page;
