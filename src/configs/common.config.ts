4f6ccaae77458db566713487dbb8f4b044f451ec
Sekhudin <sekhudinuap@gmail.com>
2025-03-06 11:21:15 +0700

    chore: minor change, has no effect

--- src/configs/common.config.ts
+++ src/configs/common.config.ts
@@ -1,2 +1,8 @@
 export const year = new Date().getFullYear();
 export const copyrightText = `Copyright KB Bank ©${year} - All rights reserved.`;
+
+export const role = {
+  user: "user",
+  developer: "developer",
+};
+export const roles = [role.user, role.developer];
