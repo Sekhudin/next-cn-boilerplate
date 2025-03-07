2a0ba30e16385e69e78745aad2c4115b2417e524
Sekhudin <sekhudinuap@gmail.com>
2025-03-07 10:56:09 +0700

    add new ui components

--- src/shared/ui/link-button.tsx
+++ src/shared/ui/link-button.tsx
@@ -1,23 +1,23 @@
 "use client";
 import * as React from "react";
-import Link, { LinkProps } from "next/link";
+import NextLink, { LinkProps } from "next/link";
 import { type VariantProps } from "class-variance-authority";
 import { buttonVariants } from "./button";
 import { cn } from "src/utils/common";
 
-export interface NextLinkProps
+export interface LinkButtonProps
   extends React.PropsWithChildren<LinkProps>,
     VariantProps<typeof buttonVariants> {
   className?: string;
 }
 
-const NextLink = React.forwardRef<HTMLAnchorElement, NextLinkProps>(
+const LinkButton = React.forwardRef<HTMLAnchorElement, LinkButtonProps>(
   ({ className, variant, size, ...props }, ref) => {
     return (
-      <Link className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      <NextLink className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
     );
   },
 );
-NextLink.displayName = "NextLink";
+LinkButton.displayName = "LinkButton";
 
-export { NextLink };
+export { LinkButton, NextLink };
